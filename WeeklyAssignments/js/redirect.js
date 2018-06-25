22379ae16df947b3480de23f0cbcf8b9c206cd6d
Costigan-Stephen <stephenjcostigan@hotmail.com>
2018-06-24 23:29:05 -0400

    Add files via upload

---
+++ WeeklyAssignments/js/redirect.js
@@ -0,0 +1,25 @@
+function redirect(x){
+    
+    switch (x){
+        case "franklin":
+            window.location.href = 'franklin-9.html';
+            break;
+        case "greenville":
+            window.location.href = 'greenville.html';
+            break;
+        case "springfield":
+            window.location.href = 'springfield.html';
+            break;
+        case "home":
+            window.location.href = 'index.html';
+            break;
+        case "storm":
+            window.location.href = 'stormcenter.html';
+            break;
+        case "gallery":
+            window.location.href = 'gallery.html';
+            break;
+        default:
+            return true;
+        }
+}
