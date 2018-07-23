e643857b52a821c0083c9e600a442d26d1b2ca17
Costigan-Stephen <stephenjcostigan@hotmail.com>
2018-07-23 17:42:01 -0400

    Add files via upload

--- termwebsite/js/include.js
+++ termwebsite/js/include.js
@@ -19,6 +19,7 @@ function include() {
                     include(); // call the function again to start the process over
                     date();
                     navCheck();
+                    
                 }
             }
             xhttp.open('GET', './modules/' + file, true); // matching file names in includes folder
@@ -27,6 +28,10 @@ function include() {
             var content = document.getElementById('gallery');  //content exists
             if (content) {
                 navCheck();
+            }
+            content = document.getElementById('services');  //content exists
+            if (content) {
+                service();
             }
 
             return;
@@ -51,4 +56,23 @@ function date() {
 }
 
 
+function service(){
+    var object = new XMLHttpRequest();
 
+    object.open('GET', 'json/services.json', true);
+
+    object.send();
+
+    object.onload = function () {
+        var objectInfo = JSON.parse(object.responseText);
+        console.log(objectInfo);
+
+        for (var i = 0; i < 6; i++) {
+            rep = i + 1;
+            document.getElementById('t' + rep + 'name').innerHTML = objectInfo.service[i].name;
+            document.getElementById('t' + rep + 'price').innerHTML = objectInfo.service[i].price;
+
+        }
+    }
+
+}
--- termwebsite/js/json.js
+++ termwebsite/js/json.js
@@ -1,23 +1,20 @@
-var weatherobject = new XMLHttpRequest();
+function service(){
+    var object = new XMLHttpRequest();
 
-weatherobject.open('GET', './json/towns.json', true);
+    object.open('GET', 'json/services.json', true);
 
-weatherobject.send();
+    object.send();
 
-weatherobject.onload = function () {
-    var weatherInfo = JSON.parse(weatherobject.responseText);
-    console.log(weatherInfo);
+    object.onload = function () {
+        var objectInfo = JSON.parse(object.responseText);
+        console.log(objectInfo);
 
-    for (var i = 0; i < 4; i++) {
-        rep = i + 1;
-        document.getElementById('t' + rep + 'name').innerHTML = weatherInfo.towns[i].name;
-        document.getElementById('t' + rep + 'motto').innerHTML = weatherInfo.towns[i].motto;
-        document.getElementById('t' + rep + 'year').innerHTML = weatherInfo.towns[i].yearFounded;
-        document.getElementById('t' + rep + 'pop').innerHTML = weatherInfo.towns[i].currentPopulation;
-        document.getElementById('t' + rep + 'rain').innerHTML = weatherInfo.towns[i].averageRainfall;
+        for (var i = 0; i < 6; i++) {
+            rep = i + 1;
+            document.getElementById('t' + rep + 'name').innerHTML = objectInfo.service[i].name;
+            document.getElementById('t' + rep + 'price').innerHTML = objectInfo.service[i].price;
 
-//        var imgcode = weatherInfo.towns[i].name;
-//        var img_path = './images/' + imgcode + ".jpg";
-//        document.getElementById('t' + rep + 'img').src = img_path;
+        }
     }
+
 }
